33a928cb6a42b532a40aec13d7b4be9ace685199
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-08-26 12:52:02 -0300

    ap01(git) teste de parametro adiconado

---
+++ teste_parametro.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void f1(int a){
+    a = a + 1;
+    printf("a dentro de f1 alterado %d\n", a);
+}
+void f2(int *a){
+    printf("conteudo referenciado pelo ponteiro a de f2: %d\n", *a);
+    *a = *a +1;
+    printf("conteudo referenciado pelo ponteiro a de f2: %d\n", *a);
+}
+int main(){
+    int a = 10;
+    f1(a);
+    printf("a depois de f1 NÃO alterado %d\n", a);
+    f2(&a);
+    printf("conteudo de a alterado na funcao f2, por referencia: %d\n", a);    
+    return 0;
+}
